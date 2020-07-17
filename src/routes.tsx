57d9bc0bb7d3bfdcafdcc6c77087402c1665c9f5
giovaniif <riccog.25@gmail.com>
2020-07-17 02:01:03 -0300

    👾 Reset User Lists

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,17 +4,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import UserList from './pages/UserList';
 import UserDetails from './pages/UserDetails';
 
-const ManyUserList: React.FC = () => (
-  <>
-    <UserList />
-    <UserList />
-    <UserList />
-  </>
-)
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={ManyUserList} />
+      <Route path="/" exact component={UserList} />
       <Route path="/users/:id" exact component={UserDetails} />
     </Switch>
   </BrowserRouter>
